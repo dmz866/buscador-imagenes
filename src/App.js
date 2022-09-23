d2446c14e7d56297787c3bbe23b4d9cce31a1c59
David Murillo <david.murillo@ascendle.com>
2022-09-22 20:07:08 -0500

    photos added

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import './components/Header/Header.css';
 
 export default function App() {
 	const [photos, setPhotos] = useState([]);
+	console.log(photos)
 	return (
 		<div>
 			<header>
@@ -14,13 +15,29 @@ export default function App() {
 					</Form>
 				</Formik>
 			</header>
+			<div className='container'>
+				<div className='center'>
+					{
+						photos.map(photo =>
+							<article key={photo.id} onClick={() => open(photo.links.html)}>
+								<img src={photo.urls.regular} />
+								<p>{[photo.description, photo.alt_description].join(' - ')}</p>
+							</article>)
+					}
+				</div>
+			</div>
 		</div>
 	);
+
+	function open(link) {
+		window.open(link);
+	}
+
+	async function search(values) {
+		const result = await fetch(`https://api.unsplash.com/search/photos?per_page=20&query=${values.search}`,
+			{ headers: { 'Authorization': 'Client-ID UI1sdidWf3xXSfhQaEjFw1jBd3KGs-ZFoR0SVkyYmL4' } });
+		const data = await result.json();
+		setPhotos(data.results);
+	}
 }
 
-async function search(values) {
-	const result = await fetch(`https://api.unsplash.com/search/photos?per_page=20&query=${values.search}`,
-		{ headers: { 'Authorization': 'Client-ID UI1sdidWf3xXSfhQaEjFw1jBd3KGs-ZFoR0SVkyYmL4' } });
-	const data = await result.json();
-	photos = data.results;
-}
