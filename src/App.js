9aaa3b0df9a27242a3d4627fc44696cfdfef0104
David Murillo <david.murillo@ascendle.com>
2022-09-22 19:56:31 -0500

    header added

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
+import { Field, Form, Formik } from 'formik';
+import { useState } from 'react';
 import './App.css';
+import './components/Header/Header.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default function App() {
+	const [photos, setPhotos] = useState([]);
+	return (
+		<div>
+			<header>
+				<Formik initialValues={{ search: '' }} onSubmit={search}>
+					<Form>
+						<Field name='search' />
+					</Form>
+				</Formik>
+			</header>
+		</div>
+	);
 }
 
-export default App;
+async function search(values) {
+	const result = await fetch(`https://api.unsplash.com/search/photos?per_page=20&query=${values.search}`,
+		{ headers: { 'Authorization': 'Client-ID UI1sdidWf3xXSfhQaEjFw1jBd3KGs-ZFoR0SVkyYmL4' } });
+	const data = await result.json();
+	photos = data.results;
+}
